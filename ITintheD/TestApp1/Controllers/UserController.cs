6897e28c1b039bdf8b187cf3de471637c79353cf
Ryan Miller1 <RyanMiller1@quickenloans.com>
2013-06-07 14:14:58 -0400

    add Mike's MVC sample web app

---
+++ ITintheD/TestApp1/Controllers/UserController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using TestApp1.Models;
+
+namespace TestApp1.Controllers
+{
+    public class UsersController : Controller
+    {
+        //
+        // GET: /Default1/
+
+        public ActionResult Index()
+        {
+            User u = new User()
+            {
+                Name = "Mike",
+                Password = "SuperSecurePassword",
+                Email = "MikePavlak@TitleSource.com",
+                UserId = 1,
+                Address = new Address()
+                {
+                    City = "Detroit",
+                    StateCode = "MI",
+                    Line1 = "600 Woodward",
+                    Zip1 = 48088
+                }
+            };
+            return View("Index", u);
+        }
+
+        //
+        // GET: /Default1/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /Default1/Create
+
+        public ActionResult Create(User u)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default1/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Default1/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default1/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /Default1/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /Default1/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
